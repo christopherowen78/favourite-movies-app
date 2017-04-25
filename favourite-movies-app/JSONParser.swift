0762a259b65bc60c1bc4d7a001f7e8ddb4f34253
Christopher Owen <Christopher.Owen@bbc.co.uk>
2017-04-25 23:19:46 +0100

    Retrieving Data from the API

---
+++ favourite-movies-app/JSONParser.swift
@@ -0,0 +1,23 @@
+//
+//  JSONParser.swift
+//  favourite-movies-app
+//
+//  Created by Christopher Owen on 25/04/2017.
+//  Copyright © 2017 Christopher Owen. All rights reserved.
+//
+
+import Foundation
+
+class JSONParser {
+    static func parse (data: Data) -> [String: AnyObject]? {
+        let options = JSONSerialization.ReadingOptions()
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: options) as? [String: AnyObject]
+            
+            return json!
+        } catch (let parseError){
+            print("There was an error parsing the JSON: \"\(parseError.localizedDescription)\"")
+        }
+        return nil
+    }
+}
